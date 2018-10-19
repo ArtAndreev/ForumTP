67893ea7433fa2dfb28ae3bea821b0617785ead7
Artyom Andreev <aandreev06.1998@gmail.com>
2018-10-19 23:03:46 +0300

    Added SET TIME ZONE (MSC), DEFAULT values for timestamps, removed unnecessary table Status

--- migrations/1_initial.sql
+++ migrations/1_initial.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS thread (
     slug varchar(64) UNIQUE,
     title varchar(128) NOT NULL,
     author integer REFERENCES forum_user NOT NULL,
-    created timestamp with time zone,
+    created timestamp with time zone DEFAULT now(),
     message text NOT NULL,
     votes integer DEFAULT 0
 );
@@ -30,10 +30,10 @@ CREATE TABLE IF NOT EXISTS thread (
 CREATE TABLE IF NOT EXISTS post (
     post_id serial PRIMARY KEY,
     forum integer REFERENCES forum NOT NULL,
-    thread integer REFERENCES thread,
+    thread integer REFERENCES thread NOT NULL,
     parent integer,
     author integer REFERENCES forum_user NOT NULL,
-    created timestamp with time zone,
+    created timestamp with time zone DEFAULT now(),
     is_edited boolean DEFAULT FALSE NOT NULL,
     message text NOT NULL
 );
@@ -45,20 +45,13 @@ CREATE TABLE IF NOT EXISTS vote (
     CONSTRAINT vote_constraint CHECK (voice IN (-1, 1))
 );
 
-CREATE TABLE IF NOT EXISTS status (
-    forum_count integer NOT NULL,
-    post_count integer NOT NULL,
-    thread_count integer NOT NULL,
-    user_count integer NOT NULL
-);
-
-INSERT INTO status VALUES 
-    (0, 0, 0, 0);
+ALTER DATABASE docker SET timezone TO 'UTC-3';
 
 -- +migrate Down
-DROP TABLE status;
 DROP TABLE vote;
 DROP TABLE post;
 DROP TABLE thread;
 DROP TABLE forum;
 DROP TABLE forum_user;
+
+ALTER DATABASE docker SET timezone TO 'UTC';
